cad7568451d27eef77db3d5943943bf4ec39267a
Shiroyaksha666 <aliesmkhani00@gmail.com>
2024-11-13 21:42:48 +0330

    fixing signup backend

--- lib/ui/signup/signup_page.dart
+++ lib/ui/signup/signup_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:full_plant_app/api/api_service.dart';
 import 'package:full_plant_app/constants/constants.dart';
-import 'package:full_plant_app/widgets/textform_widget.dart';
+import 'package:full_plant_app/models/woocommerce/register_model.dart';
 
 class SignupPage extends StatefulWidget {
   const SignupPage({super.key});
@@ -10,6 +11,17 @@ class SignupPage extends StatefulWidget {
 }
 
 class _SignupPageState extends State<SignupPage> {
+  late ApiService apiService;
+  late CustomerModel customerModel;
+  bool isApiCalled = false;
+
+  @override
+  void initState() {
+    apiService = ApiService();
+    customerModel = CustomerModel();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
@@ -58,39 +70,230 @@ class _SignupPageState extends State<SignupPage> {
               child: SizedBox(
                 height: size.height * 0.8,
                 width: size.width * 0.8,
-                child: const SingleChildScrollView(
+                child: SingleChildScrollView(
                   child: Padding(
-                    padding: EdgeInsets.all(10),
+                    padding: const EdgeInsets.all(10),
                     child: Column(
                       children: [
-                        TextFormWidget(
-                          name: 'نام',
-                          input: TextInputType.name,
-                          obscure: false,
+                        Directionality(
+                          textDirection: TextDirection.rtl,
+                          child: TextFormField(
+                            initialValue: customerModel.firstName,
+                            onChanged: (value) {
+                              customerModel.firstName = value;
+                            },
+                            keyboardType: TextInputType.name,
+                            style: const TextStyle(fontSize: 20, height: 2),
+                            cursorColor: Consts.primaryColor,
+                            textDirection: TextDirection.rtl,
+                            decoration: InputDecoration(
+                              labelText: 'نام',
+                              labelStyle: TextStyle(
+                                  color: Consts.primaryColor,
+                                  fontSize: 25,
+                                  fontWeight: FontWeight.bold),
+                              contentPadding: const EdgeInsets.all(15),
+                              focusedBorder: OutlineInputBorder(
+                                borderSide: BorderSide(
+                                  color: Consts.primaryColor,
+                                  width: 2,
+                                ),
+                              ),
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(10),
+                              ),
+                            ),
+                          ),
                         ),
-                        SizedBox(
+                        const SizedBox(
                           height: 30,
                         ),
-                        TextFormWidget(
-                            name: 'نام خانوادگی',
-                            obscure: false,
-                            input: TextInputType.name),
-                        SizedBox(
+                        Directionality(
+                          textDirection: TextDirection.rtl,
+                          child: TextFormField(
+                            initialValue: customerModel.lastName,
+                            onChanged: (value) {
+                              customerModel.lastName = value;
+                            },
+                            keyboardType: TextInputType.name,
+                            style: const TextStyle(fontSize: 20, height: 2),
+                            cursorColor: Consts.primaryColor,
+                            textDirection: TextDirection.rtl,
+                            decoration: InputDecoration(
+                              labelText: 'نام خانوادگی',
+                              labelStyle: TextStyle(
+                                  color: Consts.primaryColor,
+                                  fontSize: 25,
+                                  fontWeight: FontWeight.bold),
+                              contentPadding: const EdgeInsets.all(15),
+                              focusedBorder: OutlineInputBorder(
+                                borderSide: BorderSide(
+                                  color: Consts.primaryColor,
+                                  width: 2,
+                                ),
+                              ),
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(10),
+                              ),
+                            ),
+                          ),
+                        ),
+                        const SizedBox(
                           height: 30,
                         ),
-                        TextFormWidget(
-                            name: 'ایمیل',
-                            obscure: false,
-                            directionhint: TextDirection.ltr,
-                            input: TextInputType.emailAddress),
-                        SizedBox(
+                        Directionality(
+                          textDirection: TextDirection.rtl,
+                          child: TextFormField(
+                            initialValue: customerModel.email,
+                            onChanged: (value) {
+                              customerModel.email = value;
+                            },
+                            keyboardType: TextInputType.emailAddress,
+                            style: const TextStyle(fontSize: 20, height: 2),
+                            cursorColor: Consts.primaryColor,
+                            textDirection: TextDirection.ltr,
+                            decoration: InputDecoration(
+                              labelText: 'ایمیل',
+                              labelStyle: TextStyle(
+                                  color: Consts.primaryColor,
+                                  fontSize: 25,
+                                  fontWeight: FontWeight.bold),
+                              contentPadding: const EdgeInsets.all(15),
+                              focusedBorder: OutlineInputBorder(
+                                borderSide: BorderSide(
+                                  color: Consts.primaryColor,
+                                  width: 2,
+                                ),
+                              ),
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(10),
+                              ),
+                            ),
+                          ),
+                        ),
+                        const SizedBox(
                           height: 30,
                         ),
-                        TextFormWidget(
-                            name: 'پسورد',
-                            directionhint: TextDirection.ltr,
-                            obscure: true,
-                            input: TextInputType.visiblePassword)
+                        Directionality(
+                          textDirection: TextDirection.rtl,
+                          child: TextFormField(
+                            initialValue: customerModel.password,
+                            onChanged: (value) {
+                              customerModel.password = value;
+                            },
+                            obscureText: true,
+                            keyboardType: TextInputType.visiblePassword,
+                            style: const TextStyle(fontSize: 20, height: 2),
+                            cursorColor: Consts.primaryColor,
+                            textDirection: TextDirection.ltr,
+                            decoration: InputDecoration(
+                              labelText: 'پسورد',
+                              labelStyle: TextStyle(
+                                  color: Consts.primaryColor,
+                                  fontSize: 25,
+                                  fontWeight: FontWeight.bold),
+                              contentPadding: const EdgeInsets.all(15),
+                              focusedBorder: OutlineInputBorder(
+                                borderSide: BorderSide(
+                                  color: Consts.primaryColor,
+                                  width: 2,
+                                ),
+                              ),
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(10),
+                              ),
+                            ),
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 30,
+                        ),
+                        Row(
+                          children: <Widget>[
+                            ElevatedButton(
+                              style: ElevatedButton.styleFrom(
+                                backgroundColor: Consts.primaryColor,
+                                padding: const EdgeInsets.symmetric(
+                                  horizontal: 20,
+                                  vertical: 12,
+                                ),
+                              ),
+                              onPressed: () {
+                                apiService
+                                    .createCustomer(customerModel)
+                                    .then((retRes) {
+                                  if (retRes) {
+                                    if (context.mounted) {
+                                      showDialog(
+                                          context: context,
+                                          builder: (context) {
+                                            return AlertDialog(
+                                              title: const Text('woocommerce'),
+                                              content: const Text('succesfull'),
+                                              actions: [
+                                                TextButton(
+                                                    onPressed: () {
+                                                      Navigator.of(context)
+                                                          .pop();
+                                                    },
+                                                    child: const Text('ok'))
+                                              ],
+                                            );
+                                          });
+                                    }
+                                  } else {
+                                    if (context.mounted) {
+                                      showDialog(
+                                          context: context,
+                                          builder: (context) {
+                                            return AlertDialog(
+                                              title: const Text('woocommerce'),
+                                              content:
+                                                  const Text('not successfull'),
+                                              actions: [
+                                                TextButton(
+                                                    onPressed: () {
+                                                      Navigator.of(context)
+                                                          .pop();
+                                                    },
+                                                    child: const Text('ok'))
+                                              ],
+                                            );
+                                          });
+                                    }
+                                  }
+                                });
+                              },
+                              child: const Text(
+                                'ثبت نام',
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 20,
+                                ),
+                              ),
+                            ),
+                            const SizedBox(
+                              width: 20,
+                            ),
+                            ElevatedButton(
+                              style: ElevatedButton.styleFrom(
+                                backgroundColor: Consts.primaryColor,
+                                padding: const EdgeInsets.symmetric(
+                                  horizontal: 30,
+                                  vertical: 12,
+                                ),
+                              ),
+                              onPressed: () {},
+                              child: const Text(
+                                'قبلا اکانت ساختی؟',
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 20,
+                                ),
+                              ),
+                            )
+                          ],
+                        ),
                       ],
                     ),
                   ),
