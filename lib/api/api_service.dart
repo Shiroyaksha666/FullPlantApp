a7371dc18d217ff7ae1059db3a12aa1d2564cf6a
Shiroyaksha666 <aliesmkhani00@gmail.com>
2024-11-13 21:43:23 +0330

    login page

--- lib/api/api_service.dart
+++ lib/api/api_service.dart
@@ -1,7 +1,7 @@
 import 'dart:convert';
 import 'dart:io';
-
 import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
 import 'package:full_plant_app/constants/constants.dart';
 import 'package:full_plant_app/models/woocommerce/register_model.dart';
 
@@ -9,24 +9,28 @@ class ApiService {
   Future<bool> createCustomer(CustomerModel model) async {
     bool returnResponse = false;
 
-    String authToken = base64Encode(
+    String authToken = base64.encode(
       utf8.encode(
-          '${WoocommerceInfo.consumerKey} : ${WoocommerceInfo.consumerSecret}'),
+          '${WoocommerceInfo.consumerKey}:${WoocommerceInfo.consumerSecret}'),
     );
 
     try {
-      var response = await Dio()
-          .post(WoocommerceInfo.mainUrl + WoocommerceInfo.customerUrl,
-              data: model.toJason(),
-              options: Options(headers: {
-                HttpHeaders.authorizationHeader: 'Basic $authToken',
-                HttpHeaders.contentTypeHeader: 'application/json',
-              }));
+      var response = await Dio().post(
+        WoocommerceInfo.mainUrl + WoocommerceInfo.customerUrl,
+        data: model.toJason(),
+        options: Options(
+          headers: {
+            HttpHeaders.authorizationHeader: 'Basic $authToken',
+            HttpHeaders.contentTypeHeader: 'application/json',
+          },
+        ),
+      );
       if (response.statusCode == 201) {
         returnResponse = true;
       }
     } on DioException catch (e) {
-      if (e.response!.statusCode == 404) {
+      debugPrint('$e');
+      if (e.response?.statusCode == 404) {
         returnResponse = false;
       } else {
         returnResponse = false;
--- lib/constants/constants.dart
+++ lib/constants/constants.dart
@@ -3,11 +3,11 @@ import 'package:flutter/material.dart';
 class WoocommerceInfo {
   //ConsumerKey and ConsumerSecret
 
-  static String consumerKey = "ck_761e12dee8a7c6d041f629ed28e255c9725cf503";
-  static String consumerSecret = "cs_b23a6f05b10a99130c66a90be8b67e7078786436";
+  static String consumerKey = "ck_840b89b311e8d00aed90a27a2fcd99e332ddda95";
+  static String consumerSecret = "cs_b0191c19bac02f398cda9a8f8ee7956b6efe5587";
 
   //Base URL
-  static String mainUrl = "https://localhost/plantapp/wp-json/wc/v3/";
+  static String mainUrl = "https://plantapp.s475.site/wp-json/wc/v3/";
 
   //Endpoint URL
 
